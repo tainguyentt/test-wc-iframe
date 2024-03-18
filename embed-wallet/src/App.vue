a3a65a72a8d9af160b3fc0d920be3b5cec44f9dc
Tony Fung <tonyfung99@gmail.com>
2024-03-18 18:16:13 +0800

    trying to debug on the deficonnect object

--- embed-wallet/src/App.vue
+++ embed-wallet/src/App.vue
@@ -1,7 +1,24 @@
 <script setup lang="ts">
+
+import { ref, onMounted } from 'vue';
+
+const logs = ref("");
+
+const log = (msg: string) => {
+  logs.value += `${msg}\n`;
+};
+
+onMounted(() => {
+  log("begining, print window, ", window);
+  log("begining, print deficonnect, ", deficonnect);
+  log("begining, print window.deficonnect, ",  window.deficonnect);
+});
+
 </script>
 
 <template>
   <h1>Embedded Wallet</h1>
+  <div>Logs (outside iframe):</div>
+  <pre>{{ logs }}</pre>
   <iframe src="https://test-wallet-1.vercel.app" width="100%" height="500px"></iframe>
 </template>
