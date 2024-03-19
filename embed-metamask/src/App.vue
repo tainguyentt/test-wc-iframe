8398e539a1bf05a0d6d27c888471d7e0700dc5a5
TaiNguyen <tai.nguyen@tor.us>
2024-03-19 14:53:41 +0700

    add embed wallet connect

--- embed-metamask/src/App.vue
+++ embed-metamask/src/App.vue
@@ -2,6 +2,6 @@
 </script>
 
 <template>
-  <h1>Embedded Wallet</h1>
+  <h1>Embedded Metamask Wallet</h1>
   <iframe src="https://metamask.github.io/test-dapp/" width="100%" height="500px"></iframe>
 </template>
---
+++ embed-walletconnect/src/App.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <h1>Embedded WalletConnect</h1>
+  <iframe src="https://example.walletconnect.org/" width="100%" height="500px"></iframe>
+</template>
