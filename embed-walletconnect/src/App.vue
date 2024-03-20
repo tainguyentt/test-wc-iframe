1e15b035c44646c48dd12f1ac28bac111b648db6
TaiNguyen <tai.nguyen@tor.us>
2024-03-20 17:33:27 +0700

    disable wallet connect

--- embed-walletconnect/src/App.vue
+++ embed-walletconnect/src/App.vue
@@ -3,6 +3,6 @@
 
 <template>
   <h1>Embedded WalletConnect</h1>
-  <iframe src="https://example.walletconnect.org/" width="100%" height="300px"></iframe>
+  <!-- <iframe src="https://example.walletconnect.org/" width="100%" height="300px"></iframe> -->
   <iframe src="https://lab-walletconnect-modal.pages.dev/with-ethereum-provider" width="100%" height="300px"></iframe>
 </template>
