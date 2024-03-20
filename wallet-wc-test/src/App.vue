afcb1fa593d5ac970de8b8ce970abbd63609bf4c
TaiNguyen <tai.nguyen@tor.us>
2024-03-20 21:50:01 +0700

    update wc

--- wallet-wc-test/src/App.vue
+++ wallet-wc-test/src/App.vue
@@ -9,14 +9,20 @@ const log = (msg: unknown) => {
 };
 
 var provider = ref<Provider>();
+var session = ref<boolean>(false);
+
 onMounted(async () => {
   provider.value = await EthereumProvider.init({
     projectId: 'e7a42e53358fcfe3456a9ea4d39ddcd0',
-    showQrModal: false,
+    showQrModal: true,
     chains: [1],
     methods: ['eth_sendTransaction', 'personal_sign'],
     events: ['connect', 'disconnect']
   })
+
+  if (provider.value.session) {
+    session.value = true;
+  }
 
   provider.value.on('display_uri', handleURI)
 });
@@ -25,31 +31,43 @@ function handleURI(uri: string){
   log(uri);
 }
 
-const connect = async () => provider.value!.connect();
+const connect = async () => {
+  if (!provider.value) return;
+  await provider.value!.connect();
+  session.value = true;
+};
+
+const disconnect = async () => {
+  if (!provider.value) return;
+  await provider.value!.disconnect();
+  session.value = false;
+  logs.value = "";
+};
+
 const request = async () => {
   const addresses = await provider.value!.request({ method: 'eth_requestAccounts' });
   log(addresses);
 };
+
 const sign = async () => {
-  const addresses = await provider.value!.request({ method: 'eth_requestAccounts' });
   const rawMessageSig = await provider.value!.request({
     method: "personal_sign",
-    params: ["0x48656c6c6f20576562334d6f64616c", (addresses as string[])[0]],
+    params: ["Hello World", provider.value!.accounts[0]],
   });
   log(rawMessageSig);
 };
 </script>
 
 <template>
-  <button @click="connect">Connect</button>
-  <br />
-  <br />
-  <button @click="request">Request</button>
-  <br />
-  <br />
-  <button @click="sign">Sign</button>
-  <br />
-  <br />
+  <div v-if="session">
+    <button @click="sign">Sign</button>
+    <button @click="request">Request accounts</button>
+    <button @click="disconnect">Disconnect</button>
+  </div>
+  <div v-else>
+    <button @click="connect">Connect</button>
+  </div>
+  <br/>
   <div>Logs:</div>
   <pre>{{ logs }}</pre>
 </template>
